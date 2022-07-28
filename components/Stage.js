0634f55a68efb1779713f05cb0000a5bb8ec1fc4
Irfan <irfanalamt@gmail.com>
2022-07-28 17:29:02 -0400

    Feat: add rectangle; reset stage

--- components/Stage.js
+++ components/Stage.js
@@ -1,53 +1,119 @@
 import { Stage, Layer, Rect, Circle, Text } from 'react-konva';
 import Konva from 'konva';
 import { useState, useRef, useEffect } from 'react';
+import { Button, Container } from '@mui/material';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 const StageComponent = () => {
   const [circles, setCircles] = useState([]);
-  const [currentCircle, setCurrentCircle] = useState(null);
+  const [currentFigure, setCurrentFigure] = useState(null);
   const stageRef = useRef(null);
   const layerRef = useRef(null);
 
   useEffect(() => {
-    if (currentCircle) {
-      var circle = new Konva.Circle({
-        x: currentCircle.x,
-        y: currentCircle.y,
-        radius: 60,
-        stroke: 'black',
-        strokeWidth: 3,
-      });
-      layerRef.current.add(circle);
-      layerRef.current.draw();
+    if (currentFigure) {
+      switch (currentFigure.type) {
+        case 'circle':
+          addCircle();
+          break;
+
+        case 'rectangle':
+          addRectangle();
+          break;
+      }
     }
-  }, [currentCircle]);
+  }, [currentFigure]);
+
+  const addCircle = () => {
+    var circle = new Konva.Circle({
+      x: currentFigure.x,
+      y: currentFigure.y,
+      radius: 60,
+      stroke: 'green',
+      strokeWidth: 3,
+    });
+    layerRef.current.add(circle);
+    layerRef.current.draw();
+  };
+
+  const addRectangle = () => {
+    var rectangle = new Konva.Rect({
+      x: currentFigure.x,
+      y: currentFigure.y,
+      height: 100,
+      width: 150,
+      stroke: 'blue',
+      strokeWidth: 3,
+    });
+    layerRef.current.add(rectangle);
+    layerRef.current.draw();
+  };
+  const handleClick = () => {
+    layerRef.current.destroyChildren();
+  };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight} ref={stageRef}>
-      <Layer ref={layerRef}>
-        <Circle
-          name='draggableCircle'
-          x={50}
-          y={150}
-          radius={15}
-          fill='green'
-          draggable
-          onDragEnd={(e) => {
-            setCurrentCircle({ x: e.target.x(), y: e.target.y(), fill: 'red' });
-            setCircles((prevCircles) => [
-              ...prevCircles,
-              { x: e.target.x(), y: e.target.y(), fill: 'red' },
-            ]);
+    <Container>
+      <Stage
+        width={window.innerWidth - 100}
+        height={window.innerHeight - 200}
+        ref={stageRef}
+      >
+        <Layer>
+          <Circle
+            name='draggableCircle'
+            x={50}
+            y={150}
+            radius={15}
+            fill='black'
+            draggable
+            onDragEnd={(e) => {
+              setCurrentFigure({
+                x: e.target.x(),
+                y: e.target.y(),
+                type: 'circle',
+              });
+              setCircles((prevCircles) => [
+                ...prevCircles,
+                { x: e.target.x(), y: e.target.y() },
+              ]);
 
-            //return circle to original position
-            //  dot (.) before "draggableCircle"
-            var stage = stageRef.current;
-            var draggableCircle = stage.findOne('.draggableCircle');
-            draggableCircle.position({ x: 50, y: 150 });
-          }}
-        />
-      </Layer>
-    </Stage>
+              //return circle to original position
+              //  dot (.) before "draggableCircle"
+
+              var draggableCircle =
+                stageRef.current.findOne('.draggableCircle');
+              draggableCircle.position({ x: 50, y: 150 });
+            }}
+          />
+          <Rect
+            name='draggableRectangle'
+            x={35}
+            y={200}
+            height={20}
+            width={30}
+            fill='black'
+            draggable
+            onDragEnd={(e) => {
+              setCurrentFigure({
+                x: e.target.x(),
+                y: e.target.y(),
+                type: 'rectangle',
+              });
+              var draggableRectangle = stageRef.current.findOne(
+                '.draggableRectangle'
+              );
+              draggableRectangle.position({ x: 35, y: 200 });
+            }}
+          />
+        </Layer>
+        <Layer ref={layerRef}></Layer>
+      </Stage>
+      <Button variant='contained' onClick={() => handleClick()}>
+        RESET
+        <RestartAltIcon />
+      </Button>
+    </Container>
   );
 };
 
