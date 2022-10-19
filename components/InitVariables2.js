89c268bf1d27e73582459b5ae5d2e60203f87d72
Irfan <irfanalamt@gmail.com>
2022-10-19 19:29:54 -0400

    Style: redesign initVariables

--- components/InitVariables2.js
+++ components/InitVariables2.js
@@ -1,22 +1,22 @@
+import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+import SaveRoundedIcon from '@mui/icons-material/SaveRounded';
 import {
   Button,
+  Chip,
   List,
   ListItem,
   MenuItem,
   Select,
-  TextField,
   Tooltip,
   Typography,
 } from '@mui/material';
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import SaveRoundedIcon from '@mui/icons-material/SaveRounded';
-import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
-import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
 import { useState } from 'react';
 import { addVariableElements } from '../src/helpers';
 
-const InitVariables = ({ handleCloseDrawer, userVariables }) => {
-  const [varObj, setVarObj] = useState(userVariables || []);
+const InitVariables = ({ handleCloseDrawer, userVariables = [] }) => {
+  const [varObj, setVarObj] = useState(userVariables);
   const [varType, setVarType] = useState('prompt');
 
   function addVar() {
@@ -43,58 +43,44 @@ const InitVariables = ({ handleCloseDrawer, userVariables }) => {
   }
 
   return (
-    <>
-      <List>
-        <ListItem>
-          <Tooltip title='CLOSE'>
-            <Button
-              size='small'
-              variant='contained'
-              color='error'
-              sx={{ height: 30 }}
-              onClick={() => {
-                handleCloseDrawer();
-              }}
-            >
-              <CloseRoundedIcon sx={{ fontSize: 21 }} />
-            </Button>
-          </Tooltip>
-          <Tooltip title='SAVE'>
-            <Button
-              sx={{ height: 30, marginLeft: 1, marginRight: 'auto' }}
-              size='small'
-              variant='contained'
-              color='success'
-              onClick={saveValues}
-            >
-              <SaveRoundedIcon sx={{ fontSize: 20 }} />
-            </Button>
-          </Tooltip>
-        </ListItem>
-        <ListItem>
-          <Typography
-            sx={{
-              marginX: 'auto',
-              marginY: 1,
-              boxShadow: 1,
-              paddingX: 3,
-              paddingY: 1,
-              backgroundColor: '#b39ddb',
-              borderRadius: 1,
-            }}
-            variant='h6'
+    <List sx={{ minWidth: 350 }}>
+      <ListItem>
+        <Tooltip title='CLOSE'>
+          <Button
+            size='small'
+            variant='outlined'
+            color='error'
+            sx={{ height: 30 }}
+            onClick={handleCloseDrawer}
           >
-            Init Variables
-          </Typography>
-        </ListItem>
-        <ListItem>
-          <Typography variant='subtitle1'>Select variable type:</Typography>
+            <CloseRoundedIcon sx={{ fontSize: 21 }} />
+          </Button>
+        </Tooltip>
+        <Tooltip title='SAVE'>
+          <Button
+            sx={{ height: 30, marginLeft: 1, marginRight: 'auto' }}
+            size='small'
+            variant='outlined'
+            color='success'
+            onClick={saveValues}
+          >
+            <SaveRoundedIcon sx={{ fontSize: 20 }} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem>
+        <Chip
+          sx={{ backgroundColor: '#b39ddb', mx: 'auto', px: 2, py: 3 }}
+          label={<Typography variant='h6'>Init Variables</Typography>}
+        />
+      </ListItem>
+      <ListItem sx={{ mt: 1 }}>
+        <Tooltip title='Select variable type'>
           <Select
             value={varType}
             onChange={(e) => {
               setVarType(e.target.value);
             }}
-            sx={{ marginX: 2 }}
             size='small'
           >
             <MenuItem value='prompt'>Prompt</MenuItem>
@@ -104,55 +90,49 @@ const InitVariables = ({ handleCloseDrawer, userVariables }) => {
             <MenuItem value='month'>Month</MenuItem>
             <MenuItem value='time'>Time</MenuItem>
           </Select>
-          <Tooltip title='Add'>
-            <AddBoxRoundedIcon
-              sx={{
-                color: '#69f0ae',
-                marginX: 0.5,
-                border: '1.2px solid black',
-                width: 30,
-                height: 30,
-                padding: 0.2,
-                borderRadius: 1,
-              }}
-              onClick={addVar}
-            />
-          </Tooltip>
-          <Tooltip title='Remove'>
-            <RemoveCircleRoundedIcon
-              sx={{
-                color: '#ff5252',
-                marginX: 0.5,
-                border: '1.2px solid black',
-                width: 30,
-                height: 30,
-                padding: 0.2,
-                borderRadius: 1,
-              }}
-              onClick={removeVar}
-            />
-          </Tooltip>
-        </ListItem>
-        {/* <pre>{JSON.stringify(varObj, undefined, 2)}</pre> */}
-        <List>
-          {varObj?.map((el, i) => {
-            return addVariableElements(el.type, i, varObj, setVarObj);
-          })}
-        </List>
-        <ListItem>
-          <Typography
-            sx={{
-              color: '#e53935',
-              paddingX: 2,
-              boxShadow: 2,
-              visibility: 'hidden',
-            }}
-            id='error-box-var'
-            variant='button'
-          ></Typography>
-        </ListItem>
+        </Tooltip>
+        <Tooltip title='Add'>
+          <Button
+            sx={{ backgroundColor: '#69f0ae', ml: 4 }}
+            variant='contained'
+            size='small'
+            color='success'
+            onClick={addVar}
+          >
+            <AddBoxRoundedIcon />
+          </Button>
+        </Tooltip>
+        <Tooltip title='Remove'>
+          <Button
+            sx={{ backgroundColor: '#ff5252', ml: 2 }}
+            variant='contained'
+            size='small'
+            color='error'
+            onClick={removeVar}
+          >
+            <RemoveCircleRoundedIcon />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      {/* <pre>{JSON.stringify(varObj, undefined, 2)}</pre> */}
+      <List>
+        {varObj?.map((el, i) => {
+          return addVariableElements(el.type, i, varObj, setVarObj);
+        })}
       </List>
-    </>
+      <ListItem>
+        <Typography
+          sx={{
+            color: '#e53935',
+            paddingX: 2,
+            boxShadow: 2,
+            visibility: 'hidden',
+          }}
+          id='error-box-var'
+          variant='button'
+        ></Typography>
+      </ListItem>
+    </List>
   );
 };
 
