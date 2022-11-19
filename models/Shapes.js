39eed393af883444f65d89e585793f400b37dba6
Irfan <irfanalamt@gmail.com>
2022-11-19 12:10:42 -0500

    Feat: reset 1st shape nextItem to null when 2nd shape removed

--- models/Shapes.js
+++ models/Shapes.js
@@ -52,17 +52,69 @@ class Shapes {
     }
   }
 
+  // getConnectionsArray() {
+  //   let tempArray = [];
+  //   this.shapes.forEach((el, i) => {
+  //     // if shape has nextitem, find nextShape from shapes array, push linecordinates to tempArray
+  //     if (el.nextItem !== null) {
+  //       let index = this.shapes.findIndex((elm) => elm.id === el.nextItem);
+  //       if (index !== -1) {
+  //         let shape1 = el;
+  //         let shape2 = this.shapes[index];
+  //         console.log('valid next shape:', this.getNextValidItem(i));
+  //         // let lineColor = this.getNextValidItem(i) === null ? 'red' : 'black';
+  //         tempArray.push({
+  //           x1: shape1.getExitPoint()[0],
+  //           y1: shape1.getExitPoint()[1],
+  //           x2: shape2.getEntryPoint()[0],
+  //           y2: shape2.getEntryPoint()[1],
+  //           startItem: shape1.id,
+  //           endItem: shape2.id,
+  //           lineCap: null,
+  //           lineColor: 'black',
+  //         });
+  //       }
+  //     }
+  //     // if type is playMenu, loop through items array and find action to connect to, find shape in shape array, push linecordinates to tempArray
+  //     if (el.type === 'hexagon') {
+  //       el.userValues?.items.forEach((elm) => {
+  //         if (elm.action) {
+  //           let index = this.shapes.findIndex((s) => s.text === elm.action);
+  //           if (index !== -1) {
+  //             let shape1 = el;
+  //             let shape2 = this.shapes[index];
+  //             // let lineColor =
+  //             //   this.getNextValidItem(index) === null ||
+  //             //   el.userValues.items.some(
+  //             //     (el) => el.action !== this.getNextValidItem(index)
+  //             //   )
+  //             //     ? 'red'
+  //             //     : 'black';
+  //             tempArray.push({
+  //               x1: shape1.getExitPoint()[0],
+  //               y1: shape1.getExitPoint()[1],
+  //               x2: shape2.getEntryPoint()[0],
+  //               y2: shape2.getEntryPoint()[1],
+  //               startItem: shape1.id,
+  //               endItem: shape2.id,
+  //               lineCap: parseInt(elm.digit),
+  //             });
+  //           }
+  //         }
+  //       });
+  //     }
+  //   });
+  //   return tempArray;
+  // }
+
   getConnectionsArray() {
     let tempArray = [];
     this.shapes.forEach((el, i) => {
-      // if shape has nextitem, find nextShape from shapes array, push linecordinates to tempArray
       if (el.nextItem !== null) {
         let index = this.shapes.findIndex((elm) => elm.id === el.nextItem);
         if (index !== -1) {
           let shape1 = el;
           let shape2 = this.shapes[index];
-          console.log('valid next shape:', this.getNextValidItem(i));
-          // let lineColor = this.getNextValidItem(i) === null ? 'red' : 'black';
           tempArray.push({
             x1: shape1.getExitPoint()[0],
             y1: shape1.getExitPoint()[1],
@@ -75,38 +127,19 @@ class Shapes {
           });
         }
       }
-      // if type is playMenu, loop through items array and find action to connect to, find shape in shape array, push linecordinates to tempArray
-      if (el.type === 'hexagon') {
-        el.userValues?.items.forEach((elm) => {
-          if (elm.action) {
-            let index = this.shapes.findIndex((s) => s.text === elm.action);
-            if (index !== -1) {
-              let shape1 = el;
-              let shape2 = this.shapes[index];
-              // let lineColor =
-              //   this.getNextValidItem(index) === null ||
-              //   el.userValues.items.some(
-              //     (el) => el.action !== this.getNextValidItem(index)
-              //   )
-              //     ? 'red'
-              //     : 'black';
-              tempArray.push({
-                x1: shape1.getExitPoint()[0],
-                y1: shape1.getExitPoint()[1],
-                x2: shape2.getEntryPoint()[0],
-                y2: shape2.getEntryPoint()[1],
-                startItem: shape1.id,
-                endItem: shape2.id,
-                lineCap: parseInt(elm.digit),
-              });
-            }
-          }
-        });
-      }
     });
     return tempArray;
   }
+
   removeShape(index) {
+    // if any shapes has next item that matches the removing shape id;
+    // reset nextItem
+    let ind = this.shapes.findIndex(
+      (elm) => elm.nextItem === this.shapes[index].id
+    );
+
+    if (ind !== -1) this.shapes[ind].nextItem = null;
+
     this.shapes.splice(index, 1);
   }
 
