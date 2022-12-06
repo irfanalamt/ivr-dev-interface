0f1eef32e4bd3ab7d10be30d27d2c35560a82c08
Irfan <irfanalamt@gmail.com>
2022-12-05 21:23:36 -0500

    Refactor: use switch instead of multi ifelse

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,18 +1,14 @@
 import { Button, Typography } from '@mui/material';
 import Drawer from '@mui/material/Drawer';
-import { useEffect } from 'react';
-
 import PlayMessage from './PlayMessage3';
-
 import CallApi from './CallApi';
 import FunctionBlock from './FunctionBlock';
 import GetDigits from './GetDigits2';
+import GoToBlock from './GoToBlock';
+import PlayConfirm from './PlayConfirm';
 import PlayMenu from './PlayMenu';
 import SetParams from './SetParams';
-import PlayConfirm from './PlayConfirm';
-import GoToBlock from './GoToBlock';
 import SwitchBlock from './SwitchBlock';
-import SwitchExitPoint from './SwitchExitPoint';
 
 const DrawerComponent = ({
   isOpen,
@@ -22,120 +18,114 @@ const DrawerComponent = ({
   stageGroup,
   entireStageGroup,
 }) => {
-  useEffect(() => {
-    console.log('ue drawer');
-  }, []);
-
   const myList = () => {
-    if (shape?.type == 'roundedRectangle') {
-      return (
-        <PlayMessage
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          userVariables={userVariables}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'roundedRectangle2') {
-      return (
-        <PlayConfirm
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          userVariables={userVariables}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'invertedHexagon') {
-      return (
-        <CallApi
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          userVariables={userVariables}
-        />
-      );
-    } else if (shape?.type == 'parallelogram') {
-      return (
-        <GetDigits
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          userVariables={userVariables}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'hexagon') {
-      return (
-        <PlayMenu
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'pentagon') {
-      return (
-        <SetParams
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'rectangle') {
-      return (
-        <FunctionBlock
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          stageGroup={stageGroup}
-        ></FunctionBlock>
-      );
-    } else if (shape?.type == 'triangle') {
-      return (
-        <GoToBlock
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          entireStageGroup={entireStageGroup}
-        />
-      );
-    } else if (shape?.type == 'pentagonSwitch') {
-      return (
-        <SwitchBlock
-          shape={shape}
-          handleCloseDrawer={handleCloseDrawer}
-          userVariables={userVariables}
-          stageGroup={stageGroup}
-        />
-      );
-    } else if (shape?.type == 'tinyCircle') {
-      return (
-        <SwitchExitPoint shape={shape} handleCloseDrawer={handleCloseDrawer} />
-      );
-    } else {
-      return (
-        <>
-          <Typography sx={{ marginY: 3 }} variant='h5'>
-            Under Construction 🏗️
-          </Typography>
-          <Button
-            variant='contained'
-            sx={{ width: 200, position: 'relative', top: 200, marginX: 'auto' }}
-            onClick={handleCloseDrawer}
-          >
-            Close
-          </Button>
-        </>
-      );
+    if (!shape) return;
+
+    switch (shape.type) {
+      case 'roundedRectangle':
+        return (
+          <PlayMessage
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            userVariables={userVariables}
+            stageGroup={stageGroup}
+          />
+        );
+
+      case 'roundedRectangle2':
+        return (
+          <PlayConfirm
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            userVariables={userVariables}
+            stageGroup={stageGroup}
+          />
+        );
+
+      case 'invertedHexagon':
+        return (
+          <CallApi
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            userVariables={userVariables}
+          />
+        );
+      case 'parallelogram':
+        return (
+          <GetDigits
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            userVariables={userVariables}
+            stageGroup={stageGroup}
+          />
+        );
+      case 'hexagon':
+        return (
+          <PlayMenu
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            stageGroup={stageGroup}
+          />
+        );
+      case 'pentagon':
+        return (
+          <SetParams
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            stageGroup={stageGroup}
+          />
+        );
+      case 'rectangle':
+        return (
+          <FunctionBlock
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            stageGroup={stageGroup}
+          ></FunctionBlock>
+        );
+      case 'triangle':
+        return (
+          <GoToBlock
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            entireStageGroup={entireStageGroup}
+          />
+        );
+      case 'pentagonSwitch':
+        return (
+          <SwitchBlock
+            shape={shape}
+            handleCloseDrawer={handleCloseDrawer}
+            userVariables={userVariables}
+            stageGroup={stageGroup}
+          />
+        );
+      default:
+        return (
+          <>
+            <Typography sx={{ marginY: 3 }} variant='h5'>
+              Under Construction 🏗️
+            </Typography>
+            <Button
+              variant='contained'
+              sx={{
+                width: 200,
+                position: 'relative',
+                top: 200,
+                marginX: 'auto',
+              }}
+              onClick={handleCloseDrawer}
+            >
+              Close
+            </Button>
+          </>
+        );
     }
   };
   return (
-    <>
-      <Drawer
-        anchor='right'
-        open={isOpen}
-        // onClose={() => {
-        //   handleCloseDrawer();
-        // }}
-      >
-        {myList()}
-      </Drawer>
-    </>
+    <Drawer anchor='right' open={isOpen}>
+      {myList()}
+    </Drawer>
   );
 };
 
