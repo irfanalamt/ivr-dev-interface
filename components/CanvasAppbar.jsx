dbd241ae868db56f25e81f6e0b48d1450a19f020
Irfan <irfanalamt@gmail.com>
2023-01-20 13:14:30 -0500

    Chore: update main user guide dialog

--- components/CanvasAppbar.jsx
+++ components/CanvasAppbar.jsx
@@ -4,7 +4,7 @@ import {
   Typography,
   Button,
   Tooltip,
-  IconButton,
+  IconButton
 } from '@mui/material';
 import ArchitectureIcon from '@mui/icons-material/Architecture';
 import SaveAsIcon from '@mui/icons-material/SaveAs';
@@ -12,7 +12,7 @@ import SaveAltIcon from '@mui/icons-material/SaveAlt';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 import UserGuideDialog from './UserGuideDialog';
-import { useState } from 'react';
+import {useState} from 'react';
 
 const CanvasAppbar = ({
   isConnecting,
@@ -20,12 +20,12 @@ const CanvasAppbar = ({
   showResetDialog,
   generateFile,
   ivrName,
-  showSaveFileDialog,
+  showSaveFileDialog
 }) => {
   const [openUserGuideDialog, setOpenUserGuideDialog] = useState(false);
 
   return (
-    <Box sx={{ position: 'fixed', top: 0 }}>
+    <Box sx={{position: 'fixed', top: 0}}>
       <Box
         sx={{
           display: 'flex',
@@ -35,30 +35,27 @@ const CanvasAppbar = ({
           px: 3,
           boxShadow: 1,
           width: '100vw',
-          mx: 'auto',
-        }}
-      >
-        <Avatar sx={{ backgroundColor: '#bbdefb', mr: 2 }}>
-          <ArchitectureIcon sx={{ fontSize: '2rem', color: 'black' }} />
+          mx: 'auto'
+        }}>
+        <Avatar sx={{backgroundColor: '#bbdefb', mr: 2}}>
+          <ArchitectureIcon sx={{fontSize: '2rem', color: 'black'}} />
         </Avatar>
         <Tooltip title='user guide'>
           <IconButton
             sx={{
               mr: 2,
               backgroundColor: '#dcdcdc',
-              '&:hover': { backgroundColor: '#80cbc4' },
+              '&:hover': {backgroundColor: '#80cbc4'}
             }}
             aria-label='user-guide'
             size='small'
-            onClick={() => setOpenUserGuideDialog(true)}
-          >
-            <HelpCenterIcon sx={{ color: 'black', fontSize: 'large' }} />
+            onClick={() => setOpenUserGuideDialog(true)}>
+            <HelpCenterIcon sx={{color: 'black', fontSize: 'large'}} />
           </IconButton>
         </Tooltip>
         <Typography
-          sx={{ backgroundColor: '#c5e1a5', px: 1, boxShadow: 1, ml: 1 }}
-          variant='subtitle2'
-        >
+          sx={{backgroundColor: '#c5e1a5', px: 1, boxShadow: 1, ml: 1}}
+          variant='subtitle2'>
           {isConnecting > 0 ? 'Connect Mode' : ''}
         </Typography>
 
@@ -66,10 +63,9 @@ const CanvasAppbar = ({
           sx={{
             ml: 'auto',
             display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Typography sx={{ mr: 3 }} variant='subtitle1'>
+            alignItems: 'center'
+          }}>
+          <Typography sx={{mr: 3}} variant='subtitle1'>
             {ivrName}
           </Typography>
           <Tooltip title='SAVE TO FILE'>
@@ -78,30 +74,28 @@ const CanvasAppbar = ({
                 zIndex: 6,
                 mr: 1,
                 backgroundColor: '#dcdcdc',
-                color: 'black',
+                color: 'black'
               }}
               variant='contained'
               size='small'
               color='secondary'
-              onClick={showSaveFileDialog}
-            >
-              <SaveAsIcon sx={{ fontSize: '1.2rem' }} />
+              onClick={showSaveFileDialog}>
+              <SaveAsIcon sx={{fontSize: '1.2rem'}} />
             </Button>
           </Tooltip>
-          <Tooltip title='RESET CANVAS'>
+          <Tooltip title='RESET WORKSPACE'>
             <Button
               sx={{
                 zIndex: 6,
                 mr: 1,
                 backgroundColor: '#dcdcdc',
                 color: 'black',
-                '&:hover': { backgroundColor: '#64b5f6' },
+                '&:hover': {backgroundColor: '#64b5f6'}
               }}
               variant='contained'
               size='small'
-              onClick={showResetDialog}
-            >
-              <RestartAltIcon sx={{ fontSize: '1.2rem' }} />
+              onClick={showResetDialog}>
+              <RestartAltIcon sx={{fontSize: '1.2rem'}} />
             </Button>
           </Tooltip>
           <Tooltip title='GENERATE SCRIPT'>
@@ -111,14 +105,13 @@ const CanvasAppbar = ({
                 backgroundColor: '#66bb6a',
                 color: 'black',
                 '&:hover': {
-                  backgroundColor: '#43a047',
-                },
+                  backgroundColor: '#43a047'
+                }
               }}
               size='small'
               variant='contained'
-              onClick={generateFile}
-            >
-              <SaveAltIcon sx={{ fontSize: '1.2rem' }} />
+              onClick={generateFile}>
+              <SaveAltIcon sx={{fontSize: '1.2rem'}} />
             </Button>
           </Tooltip>
         </Box>
--- components/UserGuideDialog.jsx
+++ components/UserGuideDialog.jsx
@@ -4,31 +4,67 @@ import {
   DialogTitle,
   Typography,
   Stack,
+  List,
+  ListItem,
+  Box
 } from '@mui/material';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 
-const UserGuideDialog = ({ open, handleClose }) => {
+const UserGuideDialog = ({open, handleClose}) => {
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>
-        <Typography sx={{ display: 'flex', alignItems: 'center' }} variant='h6'>
+        <Typography sx={{display: 'flex', alignItems: 'center'}} variant='h6'>
           <HelpCenterIcon />
           User Guide
         </Typography>
       </DialogTitle>
       <DialogContent>
-        <Stack spacing={1}>
-          <Typography>
-            -Draw: drag and drop elements from the palette located on the left
-            side of the screen.
-          </Typography>
-          <Typography>
-            -Delete: drag the elements to the bottom right delete icon.
-          </Typography>
-          <Typography>
-            -Connect: right-click to switch to connect mode. To switch back to
-            drawing mode, left click anywhere on the stage or palette.
-          </Typography>
+        <Stack spacing={2}>
+          <Box>
+            <Typography fontSize='medium' variant='subtitle2'>
+              Adding Elements:
+            </Typography>
+            <List>
+              <ListItem disablePadding>
+                • To add elements to the workspace, drag and drop them from the
+                toolbar to the desired location.
+              </ListItem>
+              <ListItem disablePadding>
+                • Once an element is added, left click on it to open its
+                settings panel.
+              </ListItem>
+            </List>
+          </Box>
+          <Box>
+            <Typography fontSize='medium' variant='subtitle2'>
+              Connecting Elements:
+            </Typography>
+            <List>
+              <ListItem disablePadding>
+                • Switch to connect mode by right-clicking anywhere on the
+                workspace.
+              </ListItem>
+              <ListItem disablePadding>
+                • To connect two elements, left-click and hold down the mouse on
+                the first element and release it on the second element.
+              </ListItem>
+              <ListItem disablePadding>
+                • To switch back to drawing mode, left-click anywhere on the
+                workspace.
+              </ListItem>
+            </List>
+          </Box>
+          <Box>
+            <Typography fontSize='medium' variant='subtitle2'>
+              Deleting Elements:
+            </Typography>
+            <List>
+              <ListItem disablePadding>
+                • To delete elements, drag them to the bottom right delete icon.
+              </ListItem>
+            </List>
+          </Box>
         </Stack>
       </DialogContent>
     </Dialog>
