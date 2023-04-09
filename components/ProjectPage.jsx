e74245e77579b8b4f546144fbab893d4a4c0dc95
Irfan <irfanalamt@gmail.com>
2023-04-09 13:17:22 -0400

    Refactor: extract final code generation functions to new file

--- components/ProjectPage.jsx
+++ components/ProjectPage.jsx
@@ -2,14 +2,18 @@ import {Alert, Box, Snackbar} from '@mui/material';
 import {useEffect, useRef, useState} from 'react';
 import PromptList from '../newComponents/PromptList';
 import VariableManager from '../newComponents/VariableManager';
+import {
+  checkForStartShape,
+  findIsDefaultValuesPresent,
+  formatCode,
+  generateInitVariablesJS,
+  traverseAndReturnString,
+} from '../src/codeGeneration';
 import BottomBar from './BottomBar';
 import CanvasTest from './Canvas2';
 import CanvasAppbar2 from './CanvasAppbar2';
-import MainToolbar from './Toolbar';
-import {replaceDollarString} from '../src/myFunctions';
 import IvrDialog from './IvrDialog';
-const prettier = require('prettier');
-const babelParser = require('@babel/parser');
+import MainToolbar from './Toolbar';
 
 function ProjectPage() {
   const [selectedItemToolbar, setSelectedItemToolbar] = useState({});
@@ -223,7 +227,8 @@ function ProjectPage() {
         logDB: false
   };` + '\n \n';
 
-    const allVariablesString = generateInitVariablesJS() + '\n \n';
+    const allVariablesString =
+      generateInitVariablesJS(userVariables.current) + '\n \n';
 
     const allFunctionStringsAndDriverFunctions =
       traverseAndReturnString(startShape);
@@ -239,246 +244,6 @@ function ProjectPage() {
       endExportString;
 
     return formatCode(finalCodeString);
-  }
-
-  function formatCode(code) {
-    return prettier.format(code, {
-      parser: 'babel',
-      parser: (text, options) => babelParser.parse(text, options),
-      singleQuote: true,
-    });
-  }
-
-  function generateInitVariablesJS() {
-    const codeString = userVariables.current
-      .map((v) => {
-        const defaultValue =
-          v.type === 'number' || v.type === 'boolean'
-            ? v.defaultValue
-            : `'${v.defaultValue}'`;
-        return `this.${v.name}=${defaultValue};`;
-      })
-      .join('');
-
-    return codeString;
-  }
-
-  function findIsDefaultValuesPresent(shapes) {
-    for (let shape of shapes) {
-      const typesToIgnore = ['connector', 'jumper', 'endFlow'];
-      if (!typesToIgnore.includes(shape.type) && !shape.functionString) {
-        return shape;
-      }
-    }
-    return false;
-  }
-
-  function checkForStartShape(shapes) {
-    const startShape = shapes.find(
-      (shape) => shape.type === 'setParams' && shape.text === 'start'
-    );
-
-    return startShape;
-  }
-
-  function traverseAndReturnString(startShape) {
-    const mainMenuCode = generateMainMenuCode(startShape);
-    const visitedShapes = new Set();
-    const shapeStack = [startShape];
-    let codeAndDrivers = '';
-
-    while (shapeStack.length > 0) {
-      const currentShape = shapeStack.pop();
-      if (visitedShapes.has(currentShape)) continue;
-
-      visitedShapes.add(currentShape);
-      console.log(' ➡️' + currentShape.text);
-
-      codeAndDrivers += generateCode(currentShape);
-
-      const nextShapes = getNextShapes(currentShape);
-
-      nextShapes.forEach((shape) => {
-        shapeStack.push(shape);
-      });
-    }
-    return mainMenuCode + codeAndDrivers;
-  }
-
-  function generateCode(shape) {
-    if (shape.type === 'playMenu') {
-      return generateMenuCode(shape);
-    }
-    if (shape.type === 'switch') {
-      return generateSwitchCode(shape);
-    }
-    const typesToInclude = [
-      'setParams',
-      'playMessage',
-      'playConfirm',
-      'getDigits',
-      'runScript',
-      'callAPI',
-    ];
-    if (typesToInclude.includes(shape.type)) {
-      return shape.functionString;
-    }
-
-    return '';
-  }
-
-  function generateMenuCode(shape) {
-    let driverFunctionsString = '';
-    const items = shape.userValues?.items;
-
-    if (!items.length) {
-      return shape.functionString;
-    }
-
-    items.forEach((item) => {
-      if (item.nextItem) {
-        const shapesTillMenuOrSwitch = getShapesTillMenuOrSwitch(item.nextItem);
-        const code = `this.${shape.text}_${item.action}=async function(){
-          try{${shapesTillMenuOrSwitch
-            .map(getDriverFunctionShapeCode)
-            .join('')}}catch(err){ IVR.error('Error in ${shape.text}_${
-          item.action
-        }', err);}
-                    };`;
-        driverFunctionsString += code;
-      }
-    });
-
-    return shape.functionString + driverFunctionsString;
-  }
-
-  function generateSwitchCode(shape) {
-    const actions = shape.userValues?.actions;
-
-    if (!actions.length) {
-      return '';
-    }
-
-    let ifCode = '';
-    shape.userValues?.actions?.forEach((action) => {
-      if (action.nextItem) {
-        const actionFlowShapes = getShapesTillMenuOrSwitch(action.nextItem);
-        ifCode += `${!ifCode ? 'if' : 'else if'}(${replaceDollarString(
-          action.condition
-        )}){${actionFlowShapes.map(getDriverFunctionShapeCode).join('')}}`;
-      }
-    });
-
-    const defaultNextItem = shape.userValues.defaultActionNextItem;
-    const defaultFlowShapes = getShapesTillMenuOrSwitch(defaultNextItem);
-    const elseFlowShapesCode = defaultFlowShapes
-      ? defaultFlowShapes.map(getDriverFunctionShapeCode).join('')
-      : '';
-    const elseCode = `else{${elseFlowShapesCode}}`;
-
-    const outerCode = `this.${shape.text}= async function(){
-      try{
-        ${ifCode + elseCode}
-      }catch(err){
-        IVR.error('Error in ${shape.text}', err);
-      }
-
-    };`;
-
-    return outerCode;
-  }
-
-  function getNextShapes(shape) {
-    let nextShapes = [];
-
-    if (shape.type === 'playMenu') {
-      shape.userValues?.items?.forEach((item) => {
-        if (item.nextItem) {
-          nextShapes.push(item.nextItem);
-        }
-      });
-    } else if (shape.type === 'switch') {
-      shape.userValues?.actions?.forEach((action) => {
-        if (action.nextItem) {
-          nextShapes.push(action.nextItem);
-        }
-      });
-      if (shape.userValues?.defaultActionNextItem) {
-        nextShapes.push(shape.userValues.defaultActionNextItem);
-      }
-    } else if (
-      shape.type === 'jumper' &&
-      shape.userValues?.type === 'exit' &&
-      shape.userValues?.nextItem
-    ) {
-      nextShapes.push(shape.userValues.nextItem);
-    } else if (
-      shape.type === 'jumper' &&
-      shape.userValues?.type === 'entry' &&
-      shape.nextItem
-    ) {
-      nextShapes.push(shape.nextItem);
-    } else if (shape.nextItem) {
-      nextShapes.push(shape.nextItem);
-    }
-
-    return nextShapes;
-  }
-
-  function generateMainMenuCode(startShape) {
-    const shapesTillMenuOrSwitch = getShapesTillMenuOrSwitch(startShape);
-
-    const mainMenuString = `this.ivrMain = async function(){
-try{${shapesTillMenuOrSwitch.map(getDriverFunctionShapeCode).join('')}}
-catch(err) { IVR.error('Error in ivrMain', err); }
-    };`;
-
-    return mainMenuString;
-  }
-
-  function getShapesTillMenuOrSwitch(startShape) {
-    // Avoid shapes that are not relevant for final script
-    if (!startShape) return;
-    const typesToIgnore = ['connector', 'jumper'];
-
-    let shapesArray = [];
-    if (!typesToIgnore.includes(startShape.type)) {
-      shapesArray.push(startShape);
-    }
-
-    let nextShape = getNextShapeForSingleExit(startShape);
-
-    while (nextShape) {
-      if (!typesToIgnore.includes(nextShape.type)) {
-        shapesArray.push(nextShape);
-      }
-      nextShape = getNextShapeForSingleExit(nextShape);
-      if (!nextShape) break;
-    }
-    return shapesArray;
-  }
-  function getDriverFunctionShapeCode(shape) {
-    if (shape.type === 'endFlow') {
-      if (shape.userValues?.type === 'disconnect') {
-        return 'IVR.doDisconnect();';
-      } else if (shape.userValues?.transferPoint) {
-        return `IVR.doTransfer('${shape.userValues.transferPoint}');`;
-      }
-    } else {
-      return `await this.${shape.text}();`;
-    }
-  }
-
-  function getNextShapeForSingleExit(shape) {
-    if (
-      shape.type === 'jumper' &&
-      shape.userValues?.type === 'exit' &&
-      shape.userValues.nextItem
-    ) {
-      return shape.userValues.nextItem;
-    } else if (shape.nextItem) {
-      return shape.nextItem;
-    } else return null;
   }
 
   return (
