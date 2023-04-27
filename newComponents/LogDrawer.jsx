44c82456194d2557cb9a800cfb17db75413a124a
Irfan <irfanalamt@gmail.com>
2023-04-27 11:36:00 -0400

    Refactor: LogDrawer component for better readability

--- newComponents/LogDrawer.jsx
+++ newComponents/LogDrawer.jsx
@@ -1,14 +1,14 @@
 import {
-  Box,
-  Divider,
-  FormControlLabel,
   List,
   ListItem,
-  Radio,
-  RadioGroup,
   Stack,
-  TextField,
+  Box,
   Typography,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  TextField,
+  Divider,
 } from '@mui/material';
 
 const LogDrawer = ({logText, setLogText}) => {
@@ -23,92 +23,56 @@ const LogDrawer = ({logText, setLogText}) => {
     }));
   }
 
+  function handleRadioChange(event, type) {
+    const newType = event.target.value;
+    setLogText((prevLogText) => ({
+      ...prevLogText,
+      [type]: {
+        ...prevLogText[type],
+        type: newType,
+      },
+    }));
+  }
+
+  const renderSection = (sectionType) => (
+    <>
+      <Box sx={{display: 'flex', alignItems: 'center'}}>
+        <Typography fontSize='large' variant='subtitle2' sx={{mr: 4}}>
+          {sectionType === 'before' ? 'Before' : 'After'}
+        </Typography>
+        <RadioGroup
+          sx={{ml: 'auto'}}
+          row
+          name={`log-${sectionType}-radio-button`}
+          value={logText[sectionType].type}
+          onChange={(event) => handleRadioChange(event, sectionType)}>
+          <FormControlLabel value='trace' control={<Radio />} label='Trace' />
+          <FormControlLabel value='info' control={<Radio />} label='Info' />
+        </RadioGroup>
+      </Box>
+      <Box>
+        <TextField
+          sx={{backgroundColor: '#f5f5f5'}}
+          size='small'
+          minRows={3}
+          inputProps={{spellCheck: 'false'}}
+          fullWidth
+          multiline
+          value={logText[sectionType].text}
+          onChange={(event) => handleLogTextChange(event, sectionType)}
+        />
+      </Box>
+    </>
+  );
+
   return (
     <List>
       <ListItem sx={{mt: 2}}>
-        <Stack sx={{width: '100%'}}>
-          <Box sx={{display: 'flex', alignItems: 'center'}}>
-            <Typography fontSize='large' variant='subtitle2' sx={{mr: 4}}>
-              Before
-            </Typography>
-            <RadioGroup
-              sx={{ml: 'auto'}}
-              row
-              name='log-before-radio-button'
-              value={logText.before.type}
-              onChange={(event) => {
-                setLogText((prevLogText) => ({
-                  ...prevLogText,
-                  before: {
-                    ...prevLogText.before,
-                    type: event.target.value,
-                  },
-                }));
-              }}>
-              <FormControlLabel
-                value='trace'
-                control={<Radio />}
-                label='Trace'
-              />
-              <FormControlLabel value='info' control={<Radio />} label='Info' />
-            </RadioGroup>
-          </Box>
-          <Box>
-            <TextField
-              sx={{backgroundColor: '#f5f5f5'}}
-              size='small'
-              minRows={3}
-              inputProps={{spellCheck: 'false'}}
-              fullWidth
-              multiline
-              value={logText.before.text}
-              onChange={(event) => handleLogTextChange(event, 'before')}
-            />
-          </Box>
-        </Stack>
+        <Stack sx={{width: '100%'}}>{renderSection('before')}</Stack>
       </ListItem>
       <Divider sx={{my: 2}} />
       <ListItem>
-        <Stack sx={{width: '100%'}}>
-          <Box sx={{display: 'flex', alignItems: 'center'}}>
-            <Typography fontSize='large' variant='subtitle2' sx={{mr: 4}}>
-              After
-            </Typography>
-            <RadioGroup
-              sx={{ml: 'auto'}}
-              row
-              name='log-after-radio-button'
-              value={logText.after.type}
-              onChange={(event) => {
-                setLogText((prevLogText) => ({
-                  ...prevLogText,
-                  after: {
-                    ...prevLogText.after,
-                    type: event.target.value,
-                  },
-                }));
-              }}>
-              <FormControlLabel
-                value='trace'
-                control={<Radio />}
-                label='Trace'
-              />
-              <FormControlLabel value='info' control={<Radio />} label='Info' />
-            </RadioGroup>
-          </Box>
-          <Box>
-            <TextField
-              sx={{backgroundColor: '#f5f5f5'}}
-              size='small'
-              minRows={3}
-              inputProps={{spellCheck: 'false'}}
-              fullWidth
-              multiline
-              value={logText.after.text}
-              onChange={(event) => handleLogTextChange(event, 'after')}
-            />
-          </Box>
-        </Stack>
+        <Stack sx={{width: '100%'}}>{renderSection('after')}</Stack>
       </ListItem>
     </List>
   );
