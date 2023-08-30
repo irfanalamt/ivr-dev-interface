cb2e0934ea3606d173443dde040be4dde58f3dd8
Irfan <irfanalamt@gmail.com>
2023-08-30 09:48:48 -0400

    Chore: extend JWT expiry to 7 days

--- src/auth.js
+++ src/auth.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 
 async function createToken(payload) {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '1d',
+    expiresIn: '7d',
   });
   return token;
 }
