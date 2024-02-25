9fa1b9f9ff182e1521cd7c683b96d6ba4a211cd5
Irfan <irfanalamt@gmail.com>
2024-02-25 16:22:28 +0300

    Fix: resolve image loading issue on initial drop of elements

--- components/Canvas2.jsx
+++ components/Canvas2.jsx
@@ -76,6 +76,7 @@ const CanvasTest = ({
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     contextRef.current = context;
+    clearAndDraw();
 
     setTimeout(() => {
       clearAndDraw();
